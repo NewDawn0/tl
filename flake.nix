69358973c019cb73abdc9943c8e306a569036e07
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-03-13 15:07:55 +0100

    use nixUtils

--- flake.nix
+++ flake.nix
@@ -1,47 +1,34 @@
 {
   description = "A cli translator using google translate";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    nix-systems.url = "github:nix-systems/default";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-  };
+  inputs.utils.url = "github:NewDawn0/nixUtils";
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
-      mkPkgs = system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ inputs.rust-overlay.overlays.default ];
-        };
-    in {
-      overlays.default =
-        (final: prev: { tl = self.packages.${prev.system}.default; });
-      packages = eachSystem (system:
-        let pkgs = mkPkgs system;
-        in {
-          default = pkgs.rustPlatform.buildRustPackage {
-            pname = "tl";
-            version = "1.0.0";
-            src = ./.;
-            cargoHash = "sha256-QEEqkcsJulZtMpVZXqy5D187nA+ksRya8ggPB9YWILU=";
-            buildInputs = with pkgs;
-              [ libiconv ] ++ lib.optional stdenv.hostPlatform.isDarwin
-              darwin.apple_sdk.frameworks.SystemConfiguration;
-            meta = {
-              description =
-                "A command-line translator powered by Google Translate";
-              longDescription = ''
-                This command-line tool leverages Google Translate to translate text quickly between languages.
-                It can be used to translate command-line output, allowing users to work with any language from the terminal.
-              '';
-              homepage = "https://github.com/NewDawn0/tl";
-              license = pkgs.lib.licenses.mit;
-              maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
-              platforms = pkgs.lib.platforms.all;
-            };
-          };
-        });
+  outputs = { self, utils, ... }: {
+    overlays.default = final: prev: {
+      translate = self.packages.${prev.system}.default;
     };
+    packages = utils.lib.eachSystem { } (pkgs: {
+      default = pkgs.rustPlatform.buildRustPackage {
+        pname = "tl";
+        version = "1.0.0";
+        src = ./.;
+        useFetchCargoVendor = true;
+        cargoHash = "sha256-lWiwoBV97/i6mlqmgQbahui3t+u3Uml/MHNXzo2ytZQ=";
+        buildInputs = with pkgs;
+          [ libiconv ] ++ lib.optional stdenv.hostPlatform.isDarwin
+          darwin.apple_sdk.frameworks.SystemConfiguration;
+        meta = {
+          description = "A command-line translator powered by Google Translate";
+          longDescription = ''
+            This command-line tool leverages Google Translate to translate text quickly between languages.
+            It can be used to translate command-line output, allowing users to work with any language from the terminal.
+          '';
+          homepage = "https://github.com/NewDawn0/tl";
+          license = pkgs.lib.licenses.mit;
+          maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+          platforms = pkgs.lib.platforms.all;
+        };
+      };
+    });
+  };
 }
